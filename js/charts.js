a52663ab8cce7bb0755a8999337d0b8851793582
Julliana Bauer <bjulliana@gmx.com>
2018-11-21 18:24:23 -0500

    weather card

--- js/charts.js
+++ js/charts.js
@@ -7,7 +7,7 @@
 		var canvas = document.getElementById('historyChart');
 		var ctx    = canvas.getContext('2d');
 
-		function histDates(days) {
+        function histDates(days) {
 			return moment().add(days, 'd').toDate();
 		}
 
@@ -20,8 +20,8 @@
 				datasets: [
 					{
 						data           : [20, 50, 30, 40, 70, 100],
-						borderColor    : 'rgba(220,20,20,1)',
-						backgroundColor: 'rgba(220,20,20,0.5)'
+						backgroundColor: '#FF8373',
+                        hoverBackgroundColor: '#FF8373',
 					}
 				]
 			},
@@ -29,6 +29,9 @@
 				legend: {
 					display: false
 				},
+                tooltips        : {
+                    enabled: false
+                },
 				responsive: true,
 				scales: {
 					xAxes: [{
@@ -59,28 +62,6 @@
 					}]
 				}
 			}
-			//options: {
-			//	events          : [],
-			//	tooltips        : {
-			//		enabled: false
-			//	},
-			//	rotation        : 1 * Math.PI,
-			//	circumference   : 1 * Math.PI,
-			//	cutoutPercentage: 65,
-			//	showTooltips    : false,
-			//	animation       : {
-			//		onComplete: function () {
-			//			var width     = ctx.canvas.clientWidth;
-			//			var height    = ctx.canvas.clientHeight;
-			//			var fontSize  = ctx.canvas.clientWidth / 8 + 'px';
-			//			var color     = '#3c3c3c';
-			//			ctx.font      = 'bold ' + fontSize + ' Lato';
-			//			ctx.fillStyle = color;
-			//			ctx.fillText(light + '%', width / 2.6, height / 1.2);
-			//
-			//		}
-			//	}
-			//}
 		});
 	}
 
