50cebdf826c86109b6cfb2c22d6a8cc56f58065c
Julliana Bauer <bjulliana@gmx.com>
2018-11-26 17:04:52 -0500

    weather card mobile fix

--- js/index.js
+++ js/index.js
@@ -8,7 +8,10 @@
         dropdownToggleUser = document.querySelector('.dropdown-toggle-usr'),
         dropdownUser       = document.querySelector('.dropdown-usr'),
         readMore           = document.querySelector('.read-more'),
-        plantInfo          = document.querySelector('.plant-information');
+        plantInfo          = document.querySelector('.plant-information'),
+        weatherCard        = document.querySelector('.weather-card'),
+        frontCard          = document.querySelector('.weather-front'),
+        backCard           = document.querySelector('.weather-back');
 
     //Toggle Sidebar
     for (let i = 0; i < sidebarTrigger.length; i++) {
@@ -27,17 +30,32 @@
         dropdownUser.classList.toggle('is-open');
     });
 
-    //Toggle Read More
-    readMore.addEventListener('click', function () {
-        if (plantInfo.classList.contains('short')) {
-            plantInfo.style.maxHeight = '100%';
-            readMore.innerHTML        = 'Read Less';
-            plantInfo.classList.remove('short');
-        } else {
-            plantInfo.classList.add('short');
-            plantInfo.style.maxHeight = '100px';
-            readMore.innerHTML        = 'Read More';
+    //Toggle Dropdown
+    weatherCard.addEventListener('click', function () {
+        if (frontCard.classList.contains('active')) {
+            console.log('clicked');
+            frontCard.classList.remove('active');
+            backCard.classList.add('active');
+        } else if (!frontCard.classList.contains('active')) {
+            frontCard.classList.add('active');
+            backCard.classList.remove('active');
         }
     });
+
+    //Toggle Read More
+    if(readMore) {
+        readMore.addEventListener('click', function () {
+            if (plantInfo.classList.contains('short')) {
+                plantInfo.style.maxHeight = '100%';
+                readMore.innerHTML        = 'Read Less';
+                plantInfo.classList.remove('short');
+            } else {
+                plantInfo.classList.add('short');
+                plantInfo.style.maxHeight = '100px';
+                readMore.innerHTML        = 'Read More';
+            }
+        });
+    }
+
 
 })();
